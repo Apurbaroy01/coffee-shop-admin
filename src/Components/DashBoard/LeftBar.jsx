041185d6c42e93fe8a6dad7d0c8897ee23ee125c
Apurba Roy <apurbaroy00011@gmail.com>
2025-07-19 01:25:24 +0600

    5th

--- src/Components/DashBoard/LeftBar.jsx
+++ src/Components/DashBoard/LeftBar.jsx
@@ -5,7 +5,7 @@ const LeftBar = () => {
     return (
         <div className="flex flex-col gap-3 mb-4 w-full  **:p-2 text-center ">
             <div className="divider">
-                <h2 className=" font-bold text-center text-gray-600" id="font">Coffee Section</h2>
+                <h2 className=" font-bold text-center text-gray-500" id="font">Coffee Section</h2>
             </div>
 
 
@@ -13,11 +13,11 @@ const LeftBar = () => {
             <NavLink className="btn rounded-2xl" to="/DashBoard/updatecoffee">UpDate Coffee</NavLink>
             <NavLink className="btn rounded-2xl" to="/DashBoard/deletecoffee">Delete Coffee</NavLink>
             <div className="divider">
-                <h2 className=" font-bold text-center text-gray-600" id="font">Oder Section</h2>
+                <h2 className=" font-bold text-center text-gray-500" id="font">Oder Section</h2>
             </div>
             <NavLink className="btn rounded-2xl" to="/DashBoard/odderscoffee">Odders Coffee</NavLink>
             <div className="divider">
-                <h2 className=" font-bold text-center text-gray-600 " id="font">Oder Section</h2>
+                <h2 className=" font-bold text-center text-gray-500 " id="font">Login Section</h2>
             </div>
             <NavLink className="btn rounded-2xl" to="/DashBoard/loginhistory">Login History</NavLink>
             
