47e99692a314d329a1e625bae52dbb73cd02e99a
Apurba Roy <apurbaroy00011@gmail.com>
2025-07-19 05:03:51 +0600

    6th

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthConText } from "../../AuthProviders/AuthProviders";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/logo (2).png"
 
 const NavBar = () => {
     const { users, LogOut } = useContext(AuthConText)
@@ -22,8 +23,9 @@ const NavBar = () => {
                 users &&
                 <>
                     <div className="flex justify-between w-full items-center px-4">
-                        <div>
-                            <h2 className="text-3xl font-black"><span className="text-pink-600">T</span>-Stall</h2>
+                        <div className="flex items-center">
+                            <img className="w-10 h-auto" src={logo} alt="aa" />
+                            <h2 className="text-3xl font-black"><span className="text-pink-600">Coffee</span>-Shop</h2>
                         </div>
                         
                         <div className="flex space-x-4  py-2 items-center">
